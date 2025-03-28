bab6be2ebb5daeee908ee1903bfe81377a57d1bc
TuanAnh-LV <anhlvtse172914@fpt.edu.vn>
2025-03-28 09:00:13 +0700

    fix login

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,14 +1,43 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+
 const AdminRoute = ({ children }) => {
   const { user, role, loading } = useAuth();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [checkingAuth, setCheckingAuth] = useState(true);
 
-  if (loading) return <p>Loading...</p>;
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = localStorage.getItem("token");
+      const storedRole = localStorage.getItem("role");
 
-  return user && role === "admin" ? children : <Navigate to="/login" />;
+      if (token && storedRole === "Admin") {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+
+      setCheckingAuth(false);
+    };
+
+    checkAuth();
+  }, [user, role, loading]);
+
+  if (loading || checkingAuth) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-xl">Loading...</p>
+      </div>
+    );
+  }
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
+
 AdminRoute.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
 export default AdminRoute;
